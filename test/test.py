9b7bec7b96ee5cfe693da34ae43e2793a1ec8ceb
fybhp <617243899@qq.com>
2016-03-27 09:31:23 +0800

    加入两个函数处理错误输入

--- test/test.py
+++ test/test.py
@@ -14,23 +14,16 @@ new_s = s1+'pic/item/'+s2
 print new_s
 i = 1
 '''
-f = u'不再'
-s = u'一个'
-y = u'两个'
-z = u'三个'
-tieba_name = raw_input(u'请输入贴吧名称：\n').decode('utf-8')
-xuanze_zhuti = raw_input(u'请选择主题：\n').decode('utf-8')
-xuanze_tuce = raw_input(u'请选择图册：\n').decode('utf-8')
-def path(f,s,y):
-	dir_name=os.path.join(f,s)
-	if not os.path.exists(dir_name):
-		os.makedirs(dir_name)
-	else:
-		pass
-	dir_name2 = os.path.join(dir_name,y)
-	if not os.path.exists(dir_name2):
-		os.makedirs(dir_name2)
-	else:
-		pass
 
-path(tieba_name,xuanze_zhuti,xuanze_tuce)
+def check_xuanze_zhuti(xuanze_zhuti,zhuti):
+	add_url = ''
+	for yigezhuti in zhuti:
+		#print yigezhuti
+		if xuanze_zhuti == yigezhuti[0]:
+			add_url = yigezhuti[1]
+	if add_url == '':
+		print u'该主题不存在，请重新输入。\n'
+		xuanze_zhuti = raw_input(u'请选择主题：\n').decode('utf-8')
+		check_xuanze_zhuti(xuanze_zhuti,zhuti)
+	return add_url
+
--- tiebatupian/teibatupian.py
+++ tiebatupian/teibatupian.py
@@ -28,16 +28,35 @@ def path(f,z,y):
 	else:
 		pass
 
-def pic_download(pic_address,tieba_name,xuanze_zhuti,xuanze_tuce):
-	i = 1
-	for pic in pic_address:
-		pic1 = pic[:30]
-		pic2 = pic[-44:]
-		new_pic = pic1+'pic/item/'+pic2   #获得高清图地址
-		filename = str(i)+'.jpg'
-		print 'Downloading '+filename + '......'
-		urllib.urlretrieve(new_pic,'./'+tieba_name+'/'+xuanze_zhuti+'/'+xuanze_tuce+'/'+filename)
-		i += 1
+def check_xuanze_zhuti(xuanze_zhuti,zhuti):
+	add_url = ''
+	for yigezhuti in zhuti:
+		#print yigezhuti
+		if xuanze_zhuti == yigezhuti[0]:
+			add_url = yigezhuti[1]
+	while add_url == '':
+		print u'该主题不存在，请重新输入。\n'
+		xuanze_zhuti = raw_input(u'请选择主题：\n').decode('utf-8')
+		add_url,xuanze_zhuti = check_xuanze_zhuti(xuanze_zhuti,zhuti)
+	else:
+		return add_url,xuanze_zhuti
+
+def check_xuanze_tuce(xuanze_tuce,tuce):
+	tid = ''
+	pe = ''
+	for yigetuce in tuce:
+		#print yigezhuti
+		if xuanze_tuce == yigetuce[0]:
+			#print yigetuce[0]
+			tid = yigetuce[1]
+			pe = yigetuce[2][:-1]
+			#tid_and_pe.append([yigetuce[1],yigetuce[2][:-1]])
+	while tid  == '':
+		print u'图册不存在，请重新输入。\n'
+		xuanze_tuce = raw_input(u'请选择图册：\n').decode('utf-8')
+		tid,pe,xuanze_tuce  = check_xuanze_tuce(xuanze_tuce,tuce)
+	else:
+		return tid,pe,xuanze_tuce
 
 def get_zhuti_neirong(main_url):
 	#得到1，主题名；2，主题url；3，某主题图册数
@@ -74,33 +93,37 @@ def get_pic_address(json_url):
 		pic_addres.append(item['purl'])
 	return pic_addres
 
+def pic_download(pic_address,tieba_name,xuanze_zhuti,xuanze_tuce):
+	i = 1
+	for pic in pic_address:
+		pic1 = pic[:30]
+		pic2 = pic[-44:]
+		new_pic = pic1+'pic/item/'+pic2   #获得高清图地址
+		filename = str(i)+'.jpg'
+		print 'Downloading '+filename + '......'
+		urllib.urlretrieve(new_pic,'./'+tieba_name+'/'+xuanze_zhuti+'/'+xuanze_tuce+'/'+filename)
+		i += 1
+
 def main():
 
 	tieba_name = raw_input(u'请输入贴吧名称：\n').decode('utf-8')
 	main_url = 'http://tieba.baidu.com/photo/g?kw=' + tieba_name + '&ie=utf-8'
 	zhuti = get_zhuti_neirong(main_url)
+	if zhuti == []:
+		print u'抱歉，该贴吧图片区是空的。\n'
+		exit()
 	#print zhuti
 	for yigezhuti in zhuti:
 		print yigezhuti[0] + '   ' + yigezhuti[2] + u'个图册'
 	xuanze_zhuti = raw_input(u'请选择主题：\n').decode('utf-8')
-	for yigezhuti in zhuti:
-		#print yigezhuti
-		if xuanze_zhuti == yigezhuti[0]:
-			add_url = yigezhuti[1]
-	#print add_url[10:]
-	#else:
-		#print u'主题输入错误，请重新输入：\n'
+	add_url,xuanze_zhuti = check_xuanze_zhuti(xuanze_zhuti,zhuti)
 	zhuti_url = 'http://tieba.baidu.com/photo/g?kw=' + tieba_name + '&ie=utf-8&cat_id='+ str(add_url[10:])
 	tuce = get_tuce_neirong(zhuti_url)
 	#print tuce
 	for yigetuce in tuce:
 		print yigetuce[0] + '   ' + yigetuce[2] + u'图片'
 	xuanze_tuce = raw_input(u'请选择图册：\n').decode('utf-8')
-	for yigetuce in tuce:
-		if xuanze_tuce == yigetuce[0]:
-			tid = yigetuce[1]
-			pe = yigetuce[2][:-1]
-	#print tid，pe[:-1]
+	tid, pe,xuanze_tuce = check_xuanze_tuce(xuanze_tuce,tuce)
 	json_url = 'http://tieba.baidu.com/photo/g/bw/picture/list?kw='+tieba_name+'&alt=jview&rn=200&tid='+str(tid[3:])+'&pn=1&ps=1&pe='+str(pe)+'&info=1'
 	#print json_url
 	pic_address = get_pic_address(json_url)
